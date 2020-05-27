dbea633e35b5f4ac1459dda77c3fa0d4b4853d01
celogeek <65178+celogeek@users.noreply.github.com>
2020-05-27 11:47:46 +0200

    PKGBUILD updater

---
+++ updatepkg.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+curl -s https://api.github.com/repos/jitsi/jitsi-meet/releases/latest > RELEASE
+
+JITSI_MEET_VERSION=$(jq -r ".tag_name[18:]" RELEASE)
+JITSI_MEET_WEB_VERSION=$(jq -r .name RELEASE)
+
+cat << __EOF__
+JITSI_MEET_VERSION=$JITSI_MEET_VERSION
+JITSI_MEET_WEB_VERSION=$JITSI_MEET_WEB_VERSION
+__EOF__
+
+
+sed -i "s@^_tag_version=.*@_tag_version=$JITSI_MEET_VERSION@; s@pkgrel=.*@pkgrel=1@" */PKGBUILD
+
+sed -i "s@^_version=.*@_version=$JITSI_MEET_WEB_VERSION@" jitsi-meet-web/PKGBUILD
+
+# updpkgsums
+# makepkg --printsrcinfo > .SRCINFO
