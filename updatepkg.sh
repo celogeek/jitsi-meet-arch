f129e7f36b94d40f07e806a35b76ea6f198211d5
celogeek <65178+celogeek@users.noreply.github.com>
2020-05-27 13:39:18 +0200

    jitsi-meet-prosody

--- updatepkg.sh
+++ updatepkg.sh
@@ -19,6 +19,7 @@ __EOF__
 
 sed -i "s@^_tag_version=.*@_tag_version=$JITSI_MEET_VERSION@; s@pkgrel=.*@pkgrel=1@" */PKGBUILD
 sed -i "s@^_version=.*@_version=$JITSI_MEET_WEB_VERSION@" jitsi-meet-web/PKGBUILD
+sed -i "s@^_version=.*@_version=$JITSI_MEET_WEB_VERSION@" jitsi-meet-prosody/PKGBUILD
 sed -i "s@^_version=.*@_version=$JITSI_VIDEOBRIDGE@" jitsi-videobridge/PKGBUILD
 sed -i "s@^_version=.*@_version=$JICOFO@" jicofo/PKGBUILD
 
